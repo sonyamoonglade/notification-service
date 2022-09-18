0b2a41bfab9524092714df5c067e4a7618cf9507
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-09-18 15:18:32 +0300

    fixed header set bug

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -26,22 +26,23 @@ func NoContent(w http.ResponseWriter) {
 }
 
 func Internal(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusInternalServerError)
 	w.Header().Add("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusInternalServerError)
 	w.Write([]byte("Internal error"))
 	return
 }
 
 func Json(logger *zap.SugaredLogger, w http.ResponseWriter, code int, content JSON) {
-
-	w.Header().Add("Content-Type", "application/json")
 	bytes, err := json.Marshal(content)
 	if err != nil {
 		logger.Error(err.Error())
 		Internal(w)
 		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
+
 	w.Write(bytes)
 	return
 }
@@ -52,8 +53,8 @@ func Binary(w http.ResponseWriter, buff []byte, mime string) {
 		ct = mime
 	}
 
+	w.Header().Set("Content-Type", ct)
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(buff)))
 	w.WriteHeader(http.StatusOK)
-	w.Header().Add("Content-Type", ct)
-	w.Header().Add("Content-Length", fmt.Sprintf("%d", len(buff)))
 	w.Write(buff)
 }
